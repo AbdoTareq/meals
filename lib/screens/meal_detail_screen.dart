64a79dcf6d353a729dda471a3a043d856bcdc59d
Abdo Tareq <abdotarekfathy@gmail.com>
2020-08-24 14:33:56 +0200

    feat: Add meal details

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,17 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/dummy_data.dart';
+import 'package:meals/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
   @override
   Widget build(BuildContext context) {
-    final mealId = ModalRoute.of(context).settings.arguments as String;
+    final mId = ModalRoute.of(context).settings.arguments as String;
+    final meal = DUMMY_MEALS.firstWhere((element) => element.id == mId);
+
+    Widget buildTitle(String title) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      );
+    }
+
+    Widget buildList(Widget child) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                border: Border.all(color: Colors.grey)),
+            width: double.infinity,
+            height: 200,
+            child: child),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meal detail'),
+        title: Text('${meal.title}'),
       ),
-      body: Center(child: Text(mealId)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image.network(meal.imageUrl),
+            buildTitle('Ingredients'),
+            buildList(Card(
+              elevation: 10,
+              // be careful u can't put list view directly inside column as both have infinity height
+              child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) => Container(
+                    color: Theme.of(context).accentColor,
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    child: Text(meal.ingredients[index])),
+              ),
+            )),
+            buildTitle('Steps'),
+            buildList(Card(
+              elevation: 10,
+              // be careful u can't put list view directly inside column as both have infinity height
+              child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) => Container(
+                    child: Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          '# ${index + 1}',
+                        ),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    Divider()
+                  ],
+                )),
+              ),
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
